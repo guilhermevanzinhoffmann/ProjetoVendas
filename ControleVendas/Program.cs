8513d225a9e351f9dce3dbdee211b3f69d5cfa5c
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2022-12-17 16:55:31 -0300

    Configurar utilização de autenticação em Program.cs

--- ControleVendas/Program.cs
+++ ControleVendas/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace ControleVendas
 {
     public class Program
@@ -13,6 +17,26 @@ namespace ControleVendas
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata= false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -24,6 +48,7 @@ namespace ControleVendas
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
