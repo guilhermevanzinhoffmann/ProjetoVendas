0968ec4826185d2cb1369ef90e22bf4563105e63
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2022-12-17 16:39:35 -0300

    Criação de token para autenticação

---
+++ ControleVendas/Services/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using ControleVendas.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ControleVendas.Services
+{
+    public static class TokenService
+    {
+        private static readonly int _hourExpires = 4;
+        
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(_hourExpires),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ ControleVendas/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace ControleVendas
+{
+    public static class Settings
+    {
+        public static readonly string Secret = "218ba5c069@fb4c6dbae85b7f4f4c14#b5d1#8cd9a7e71d4d479d0eaf85e8191632";
+    }
+}
