a08285f5443cfaa713f7ea7544745b30739f0939
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2022-12-19 10:45:45 -0300

    Incluir image postgres docker-compose

--- ControleVendas/Controllers/WeatherForecastController.cs
+++ ControleVendas/Controllers/WeatherForecastController.cs
@@ -22,15 +22,9 @@ namespace ControleVendas.Controllers
        
         [HttpGet(Name = "GetWeatherForecast")]
         [Authorize]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return User.Identity.Name;
         }
     }
 }
