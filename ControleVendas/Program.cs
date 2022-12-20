0cafb73f8e46a4e783a6347cfc4354ec9b07ac74
Guilherme Vanzin Hoffmann <guilhermevanzin@gmail.com>
2022-12-20 11:43:46 -0300

    Ajustar conexão container postgres

--- ControleVendas/Program.cs
+++ ControleVendas/Program.cs
@@ -1,10 +1,10 @@
+using ControleVendas.DBManager;
+using ControleVendas.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Diagnostics.Metrics;
-using System.Reflection.Metadata;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ControleVendas
 {
@@ -17,6 +17,15 @@ namespace ControleVendas
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            var connectionString = builder.Configuration.GetConnectionString("postgres");
+
+            builder.Services.AddDbContext<SalesContext>(options =>
+                options.UseNpgsql(connectionString)
+            );
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -91,6 +100,8 @@ namespace ControleVendas
             app.MapControllers();
 
             app.Run();
+
+           
         }
     }
 }
